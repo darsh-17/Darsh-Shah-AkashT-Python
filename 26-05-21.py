74e3c188fda049c508792995e2af17f675188dfb
darsh-17 <84734038+darsh-17@users.noreply.github.com>
2021-05-28 10:27:23 +0530

    Add files via upload

---
+++ 26-05-21.py
@@ -0,0 +1,17 @@
+a=b=c=10 #Same value assigned to different variables
+d,e,f=10,19,"Sad" #Assigned three different values to three variables in a single line
+s="AkashTech" #String
+l=["Python","Django","Strings","List","Dictionary",3.9] #List
+t=(5,1,"Java","ASP",90) #Tuple
+mydict={1:"Name","day":2,3:"Happy"} #Dictionary
+print(s[0])
+print(s[1:4])
+print(s[2:])
+print(s[:7])
+print(l[2])
+print(l[3:5])
+print(l[:5])
+print(l[3:])
+#t[2]="C" Not possible as tuple is immutable. Rest all operations similar to list
+print(mydict["day"])
+print(mydict[1])
